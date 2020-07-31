7b18cce43acfab380ed8134399c7180e1b98b4b6
lothaire <lothaire.noah@epitech.eu>
2020-07-31 11:57:56 +0200

    add player and custmisation menu

---
+++ FPF/Assets/Player/Source/Scripts/PlayerSettings.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSettings : MonoBehaviour
+
+{
+    public Texture[] texture;
+    public int currentTexture;
+    public GameObject Player;
+    private 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void NextSkin()
+    {
+        currentTexture++;
+        currentTexture %= texture.Length;
+        Player.GetComponent<Renderer>().material.mainTexture = texture[currentTexture];
+    }
+
+    public void PreviousSkin()
+    {
+        currentTexture--;
+        if (currentTexture < 0)
+            currentTexture = texture.Length -1 ;
+        else
+            currentTexture %= texture.Length;
+        Player.GetComponent<Renderer>().material.mainTexture = texture[currentTexture];
+    }
+
+
+}
